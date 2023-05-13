ccc7511929c048ee157c01bbd4fd214e83ba268e
Aguinaldo Moreira <aguinaldo.moreira@senior.com.br>
2023-05-13 17:19:13 -0300

    add authorization token

--- Api.js
+++ Api.js
@@ -7,7 +7,8 @@ const api = axios.create({
     headers: {
        'Accept': 'application/vnd.github+json',
        'Content-Type': 'application/json',
-       'X-GitHub-Api-Version': '2022-11-28'
+       'X-GitHub-Api-Version': '2022-11-28',
+       'Authorization': 'Bearer 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'
      },
 })
 
